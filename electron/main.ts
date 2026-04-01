03629f85b2688fe1259e0be0c288213f625593e3
Pin-Jung Chen <pjchen9413@gmail.com>
2026-04-01 00:18:26 -0700

    fix: remove automatic opening of developer tools in dev mode

--- electron/main.ts
+++ electron/main.ts
@@ -745,7 +745,6 @@ function createWindow() {
     // Load the app
     if (process.env.VITE_DEV_SERVER_URL) {
         mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL)
-        mainWindow.webContents.openDevTools()
     } else {
         mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
     }
